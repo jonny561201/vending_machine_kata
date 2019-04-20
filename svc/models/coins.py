aee0633c6b2ff3d22a5a6b746f2e072706049308
Jon Graf <jgraf@leantechniques.com>
2019-04-20 11:10:22 -0500

    created list of valid coins

--- svc/models/coins.py
+++ svc/models/coins.py
@@ -3,23 +3,31 @@ NICKEL = {
     'height': 21.21,
     'value': 0.05
 }
+
 DIME = {
     'weight': 2.268,
     'height': 17.91,
     'value': 0.10
 }
+
 QUARTER = {
     'weight': 5.67,
     'height': 24.26,
     'value': 0.25
 }
+
 HALF_DOLLAR = {
     'weight': 11.34,
     'height': 30.61,
     'value': 0.50
 }
+
 DOLLAR = {
     'weight': 8.1,
     'height': 26.5,
     'value': 1.0
 }
+
+VALID_COINS = [
+    NICKEL
+]
