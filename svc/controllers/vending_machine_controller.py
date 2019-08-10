8df32c001186b0c28d45866ec1b3a060056a480f
Jon Graf <jgraf@leantechniques.com>
2019-08-10 15:34:31 -0500

    controller will now call out to accumulate funds

--- svc/controllers/vending_machine_controller.py
+++ svc/controllers/vending_machine_controller.py
@@ -1,7 +1,8 @@
-from svc.services.coin import has_sufficient_funds
+from svc.services.coin import has_sufficient_funds, count_funds
 
 
 def controller(selection, inserted_coins):
+    amount = count_funds(None)
     if not has_sufficient_funds(None, None):
         return {'message': 'Insufficient funds supplied!'}
     return {'message': 'Thank you!'}
--- test/unit/controllers/controller_tests.py
+++ test/unit/controllers/controller_tests.py
@@ -3,15 +3,25 @@ from mock import patch
 from svc.controllers.vending_machine_controller import controller
 
 
+@patch('svc.controllers.vending_machine_controller.count_funds')
 @patch('svc.controllers.vending_machine_controller.has_sufficient_funds')
-def test_controller__should_call_has_sufficient_funds(mock_funds):
+def test_controller__should_call_has_sufficient_funds(mock_funds, mock_count):
     controller(None, None)
 
     mock_funds.assert_called_once()
 
 
+@patch('svc.controllers.vending_machine_controller.count_funds')
 @patch('svc.controllers.vending_machine_controller.has_sufficient_funds')
-def test_controller__should_return_message_for_insufficient_funds(mock_funds):
+def test_controller__should_call_count_funds(mock_funds, mock_count):
+    controller(None, None)
+
+    mock_count.assert_called_once()
+
+
+@patch('svc.controllers.vending_machine_controller.count_funds')
+@patch('svc.controllers.vending_machine_controller.has_sufficient_funds')
+def test_controller__should_return_message_for_insufficient_funds(mock_funds, mock_count):
     mock_funds.return_value = False
 
     actual = controller(None, None)
@@ -19,8 +29,9 @@ def test_controller__should_return_message_for_insufficient_funds(mock_funds):
     assert actual['message'] == 'Insufficient funds supplied!'
 
 
+@patch('svc.controllers.vending_machine_controller.count_funds')
 @patch('svc.controllers.vending_machine_controller.has_sufficient_funds')
-def test_controller__should_return_success_message_when_able_to_purchase(mock_funds):
+def test_controller__should_return_success_message_when_able_to_purchase(mock_funds, mock_count):
     mock_funds.return_value = True
 
     actual = controller(None, None)
