3fec86d5566704b96bd6335d49356dafef7915c1
Jon Graf <jgraf@leantechniques.com>
2019-08-10 14:51:54 -0500

    refactored tests to be more simplified

--- test/unit/controllers/controller_tests.py
+++ test/unit/controllers/controller_tests.py
@@ -5,10 +5,6 @@ from svc.controllers.vending_machine_controller import controller
 
 @patch('svc.controllers.vending_machine_controller.has_sufficient_funds')
 def test_controller__should_call_has_sufficient_funds(mock_funds):
-    inserted_coins = []
-    selection = 'H3'
-    mock_funds.return_value = False
-
-    controller(selection, inserted_coins)
+    controller(None, None)
 
     mock_funds.assert_called_once()
