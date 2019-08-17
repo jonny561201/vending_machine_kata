0bb8b19c40bd2eb07fc5e8d0cb031e65a41bb016
Jon Graf <jongraf1@gmail.com>
2019-08-16 20:24:30 -0500

    fixed issue with vending machine controller not instantiating product service

--- svc/controllers/vending_machine_controller.py
+++ svc/controllers/vending_machine_controller.py
@@ -1,9 +1,9 @@
 from svc.services import coin_service
-from svc.services import product_service
+from svc.services.product_service import ProductService
 
 
 def controller(selection, inserted_coins):
-    if not product_service.is_product_available(selection):
+    if not ProductService(None).is_product_available(selection):
         return {'message': 'Product is unavailable!'}, False
     valid_coins = [valid_coin for valid_coin in inserted_coins if coin_service.is_valid_coin(valid_coin)]
     amount = coin_service.count_funds(valid_coins)
--- test/unit/controllers/test_controller.py
+++ test/unit/controllers/test_controller.py
@@ -4,7 +4,7 @@ from svc.controllers.vending_machine_controller import controller
 from svc.models.coins import DIME, QUARTER
 
 
-@patch('svc.controllers.vending_machine_controller.product_service')
+@patch('svc.controllers.vending_machine_controller.ProductService')
 @patch('svc.controllers.vending_machine_controller.coin_service')
 def test_controller__should_call_has_sufficient_funds(mock_coin, mock_product):
     controller(None, [QUARTER])
@@ -12,7 +12,7 @@ def test_controller__should_call_has_sufficient_funds(mock_coin, mock_product):
     mock_coin.has_sufficient_funds.assert_called_once()
 
 
-@patch('svc.controllers.vending_machine_controller.product_service')
+@patch('svc.controllers.vending_machine_controller.ProductService')
 @patch('svc.controllers.vending_machine_controller.coin_service')
 def test_controller__should_call_count_funds(mock_coin, mock_product):
     controller(None, [QUARTER])
@@ -20,7 +20,7 @@ def test_controller__should_call_count_funds(mock_coin, mock_product):
     mock_coin.count_funds.assert_called_once()
 
 
-@patch('svc.controllers.vending_machine_controller.product_service')
+@patch('svc.controllers.vending_machine_controller.ProductService')
 @patch('svc.controllers.vending_machine_controller.coin_service')
 def test_controller__should_call_is_valid_coin(mock_coin, mock_product):
     controller(None, [QUARTER])
@@ -28,15 +28,15 @@ def test_controller__should_call_is_valid_coin(mock_coin, mock_product):
     mock_coin.is_valid_coin.assert_called_once()
 
 
-@patch('svc.controllers.vending_machine_controller.product_service')
+@patch('svc.controllers.vending_machine_controller.ProductService')
 @patch('svc.controllers.vending_machine_controller.coin_service')
 def test_controller__should_call_is_product_available(mock_coin, mock_product):
     controller(None, [QUARTER])
 
-    mock_product.is_product_available.assert_called_once()
+    mock_product.return_value.is_product_available.assert_called_once()
 
 
-@patch('svc.controllers.vending_machine_controller.product_service')
+@patch('svc.controllers.vending_machine_controller.ProductService')
 @patch('svc.controllers.vending_machine_controller.coin_service')
 def test_controller__should_return_message_for_insufficient_funds(mock_coin, mock_product):
     mock_coin.has_sufficient_funds.return_value = False
@@ -47,7 +47,7 @@ def test_controller__should_return_message_for_insufficient_funds(mock_coin, moc
     assert actual['message'] == 'Insufficient funds supplied!'
 
 
-@patch('svc.controllers.vending_machine_controller.product_service')
+@patch('svc.controllers.vending_machine_controller.ProductService')
 @patch('svc.controllers.vending_machine_controller.coin_service')
 def test_controller__should_return_success_message_when_able_to_purchase(mock_coin, mock_product):
     mock_coin.has_sufficient_funds.return_value = True
@@ -58,7 +58,7 @@ def test_controller__should_return_success_message_when_able_to_purchase(mock_co
     assert actual['message'] == 'Thank you!'
 
 
-@patch('svc.controllers.vending_machine_controller.product_service')
+@patch('svc.controllers.vending_machine_controller.ProductService')
 def test_controller__should_return_success_when_using_real_services(mock_product):
     funds = [QUARTER, QUARTER, DIME]
     selection = 'B10'
@@ -69,7 +69,7 @@ def test_controller__should_return_success_when_using_real_services(mock_product
     assert actual['message'] == 'Thank you!'
 
 
-@patch('svc.controllers.vending_machine_controller.product_service')
+@patch('svc.controllers.vending_machine_controller.ProductService')
 def test_controller__should_return_success_when_using_real_services_and_coin_without_value(mock_product):
     funds = [{'weight': QUARTER['weight'], 'diameter': QUARTER['diameter']}]
     selection = 'B10'
@@ -80,7 +80,7 @@ def test_controller__should_return_success_when_using_real_services_and_coin_wit
     assert actual['message'] == 'Thank you!'
 
 
-@patch('svc.controllers.vending_machine_controller.product_service')
+@patch('svc.controllers.vending_machine_controller.ProductService')
 def test_controller__should_return_insufficient_funds_when_using_real_services_and_invalid_coins(mock_product):
     funds = [{'weight': 18.28, 'diameter': 92.76}]
     selection = 'B10'
